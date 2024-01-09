683a6e3a6eccb6777ef6d4d5ef52b2219dabebb2
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-01-09 14:06:15 +0900

    init : tailwindCSS 초기 설정 및 globalCSS 1차 적용

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+//tailwindcss 설정
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
