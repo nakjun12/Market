7a9480e8bcd40f46e73ab90c13b7e7eab2f72b0c
Sunghum Paik (Brian) <zerosial@gmail.com>
2024-01-16 16:40:15 +0900

    feat: 로그인, 회원가입 화면 개발

--- src/pages/login/components/LoginForm.js
+++ src/pages/login/components/LoginForm.js
@@ -1,4 +1,4 @@
-export const JoinForm = () => {
+export const LoginForm = () => {
   return (
     <div>
       <section className="bg-base-50 dark:bg-base-900">
@@ -6,24 +6,9 @@ export const JoinForm = () => {
           <div className="w-full bg-base-200 rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                회원가입
+                로그인
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
-                <div>
-                  <label
-                    htmlFor="username"
-                    className="block mb-2 text-sm font-medium text-base-900">
-                    User name
-                  </label>
-                  <input
-                    type="username"
-                    name="username"
-                    id="username"
-                    placeholder="Brian"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    required=""
-                  />
-                </div>
                 <div>
                   <label
                     htmlFor="email"
@@ -54,32 +39,18 @@ export const JoinForm = () => {
                     required=""
                   />
                 </div>
-                <div>
-                  <label
-                    htmlFor="confirm-password"
-                    className="block mb-2 text-sm font-medium text-base-900">
-                    Confirm password
-                  </label>
-                  <input
-                    type="confirm-password"
-                    name="confirm-password"
-                    id="confirm-password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    required=""
-                  />
-                </div>
+
                 <button
                   type="submit"
                   className="w-full text-gray-700 bg-base-300 hover:bg-base-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                  가입하기
+                  로그인
                 </button>
                 <p className="text-sm font-light text-base-500">
-                  이미 회원 가입하셨나요?{" "}
+                  가입을 하셔야 하나요?{"  "}
                   <a
                     href="#"
                     className="text-blue-500 font-black hover:underline">
-                    Login here
+                    Join here
                   </a>
                 </p>
               </form>
