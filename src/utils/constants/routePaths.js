036791b4108a9b41c5a2a30024ad5fe2b50cecc7
Brian. Paik <zerosial@gmail.com>
2024-01-19 16:19:27 +0900

    feat: 프록시 및 cookie와 cors 해결을 위한 임시 설정

--- src/utils/constants/routePaths.js
+++ src/utils/constants/routePaths.js
@@ -12,18 +12,18 @@
  * <Route path={ROUTES.PRODUCT} component={ProductPage} />
  */
 export const ROUTES = {
-  HOME: "/", // 홈페이지 라우트
-  LOGIN: "/login", // 로그인 페이지 라우트
-  JOIN: "/join", // 회원가입 페이지 라우트
-  PRODUCT: "/product/:productid", // 개별 제품 상세 페이지 라우트 (동적 파라미터)
-  PRODUCTS: "/products", // 제품 목록 페이지 라우트
-  MYPAGE: "/mypage", // 사용자 마이페이지 라우트
-  BASKET: "/Basket", // 장바구니 페이지 라우트
-  TEST_COUNTER: "/test/counter", // 테스트용 카운터 페이지 라우트
-  ALL_PRODUCTS: "/products/all", // 모든 제품 목록 페이지 라우트
-  CATEGORY_PRODUCTS: "/products/:categoryId", // 카테고리별 제품 목록 페이지 라우트 (동적 파라미터)
+  HOME: "/web/", // 홈페이지 라우트
+  LOGIN: "/web/login", // 로그인 페이지 라우트
+  JOIN: "/web/join", // 회원가입 페이지 라우트
+  PRODUCT: "/web/product/:productid", // 개별 제품 상세 페이지 라우트 (동적 파라미터)
+  PRODUCTS: "/web/products", // 제품 목록 페이지 라우트
+  MYPAGE: "/web/mypage", // 사용자 마이페이지 라우트
+  BASKET: "/web/basket", // 장바구니 페이지 라우트
+  TEST_COUNTER: "/web/test/counter", // 테스트용 카운터 페이지 라우트
+  ALL_PRODUCTS: "/web/products/all", // 모든 제품 목록 페이지 라우트
+  CATEGORY_PRODUCTS: "/web/products/:categoryId", // 카테고리별 제품 목록 페이지 라우트 (동적 파라미터)
   NOT_FOUND: "*", // 일치하는 라우트가 없을 때 표시될 페이지 (404 페이지 등)
-  POST: "/post", // 게시물 생성 또는 표시 페이지 라우트
-  TEST: "/test", // 테스트 페이지 라우트
-  AUTH: "/auth" // 인증 관련 페이지 라우트
+  POST: "/web/post", // 게시물 생성 또는 표시 페이지 라우트
+  TEST: "/web/test", // 테스트 페이지 라우트
+  AUTH: "/web/auth" // 인증 관련 페이지 라우트
 };
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig, transformWithEsbuild } from "vite";
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/web/",
   plugins: [
     //jsx -> js 변환 세팅
     {
@@ -39,6 +40,24 @@ export default defineConfig({
     //절대 경로 세팅
     alias: {
       "@": resolve(currentDir, "src") // '@'를 src 폴더의 절대 경로로 설정
+    }
+  },
+  preview: {
+    port: 8080,
+    strictPort: true
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+    proxy: {
+      // 모든 요청을 https://pinemarket.cielui.com/web 으로 프록시합니다.
+      "/": {
+        target: "https://pinemarket.cielui.com",
+        changeOrigin: true,
+        secure: false
+      }
     }
   }
 });
