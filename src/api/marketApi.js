4bcb02a98ae6d9d7553f7857e93e22269c5a98ef
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-01-22 14:56:53 +0900

    fix : axios 인스턴스 수정

--- src/api/marketApi.js
+++ src/api/marketApi.js
@@ -71,7 +71,7 @@ export const postPosts = (postData) => {
 
 // 검색 조건에 따라 게시물 가져오기
 export const getPublishedPosts = (page, limit, query, orderBy, direction) => {
-  return postsApi.get("/posts/published", {
+  return marketApi.get("/posts/published", {
     params: {
       page,
       limit,
