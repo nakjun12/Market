d29d20af45a5d93db1fc66501f6129634a782b2b
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-01-25 17:01:48 +0900

    cl : path import 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
