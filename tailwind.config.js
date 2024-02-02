9f2e4d6967481bb62192650bc468c032b25a1c56
Sunghum Paik (Brian) <zerosial@gmail.com>
2024-02-02 13:08:40 +0900

    feat: PC 모드 전체 스크롤 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,26 @@ export default {
     }
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    // 글로벌 스타일을 추가하는 플러그인
+    function ({ addBase }) {
+      addBase({
+        html: {
+          "&::-webkit-scrollbar": {
+            display: "none"
+          },
+          "scrollbar-width": "none" // Firefox
+        },
+        body: {
+          "&::-webkit-scrollbar": {
+            display: "none"
+          },
+          "scrollbar-width": "none" // Firefox
+        }
+      });
+    }
+  ],
   daisyui: {
     themes: ["cupcake"]
   }
