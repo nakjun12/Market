5d2577f02395cbc5268df85ab0778722259d927d
Sunghum Paik (Brian) <zerosial@gmail.com>
2024-02-07 08:23:14 +0900

    refactor: auth 통신 성공시에만 초기화하게 변경

--- src/utils/hooks/store/useAuthStore.js
+++ src/utils/hooks/store/useAuthStore.js
@@ -19,18 +19,19 @@ const useAuthStore = create(
     logout: async () => {
       try {
         // 서버의 로그아웃 엔드포인트에 요청
-        await marketApi.post("/auth/logout");
+        const res = await marketApi.post("/auth/logout");
+        if (res.status === 200) {
+          set({
+            accessToken: null,
+            isAuthenticated: false,
+            userName: null,
+            id: null,
+            email: null
+          });
+        }
       } catch (error) {
         console.error("Logout failed:", error);
       }
-      // 클라이언트 상태 업데이트
-      set({
-        accessToken: null,
-        isAuthenticated: false,
-        userName: null,
-        id: null,
-        email: null
-      });
     }
   }))
 );
